cb2800ed9a0cc0df124f1bdc0ecaa97559ecbed1
Ben Cyrus <bencyrus@Bens-MacBook-Air.local>
2023-09-25 20:33:53 -0400

    added posts page

--- frontend/src/pages/PostsPage.tsx
+++ frontend/src/pages/PostsPage.tsx
@@ -8,15 +8,37 @@ const PostsPage = ({ posts }: PostsPageProps) => {
     return (
         <div>
             <h1>Posts Page</h1>
-            <ul>
-                {posts.map((post) => (
-                    <li>
-                        <h5>{post.title}</h5>
-                        <p>{post.summary}</p>
-                        <p>{post.body}</p>
-                    </li>
-                ))}
-            </ul>
+            {posts.map((post) => (
+                <div
+                    style={{
+                        width: '100%',
+                        maxWidth: '800px',
+                        margin: '0 auto',
+                        backgroundColor: '#ededed',
+                        padding: '10px',
+                        borderRadius: '5px',
+                        boxSizing: 'border-box',
+                        marginBottom: '20px',
+                    }}>
+                    <header>
+                        <h1>{post?.title}</h1>
+                        <div style={{ fontStyle: 'italic', marginBottom: '10px' }}>
+                            Published on{' '}
+                            {new Date(post.created_at).toLocaleDateString('en-US', {
+                                year: 'numeric',
+                                month: 'long',
+                                day: 'numeric',
+                                hour: '2-digit',
+                                minute: '2-digit',
+                            })}
+                        </div>
+                        <hr />
+                        <p>{post?.summary}</p>
+                    </header>
+
+                    {post?.body && <div dangerouslySetInnerHTML={{ __html: post.body }} />}
+                </div>
+            ))}
         </div>
     )
 }
